c5f6a718a9ac95b9ae9f53595246ed24343b2afd
Dennis Wronka <dennis@notebook.dennis>
2025-08-24 12:53:34 +0200

    Add DoubleColumn, MediumIntColumn, SmallIntColumn, and TinyIntColumn.

---
+++ src/TableColumns/MediumIntColumn.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace unrealization\TableColumns;
+
+class MediumIntColumn extends GenericColumn
+{
+	public function __construct(string $name, bool $unsigned = false, bool $nullable = false, bool $autoIncrement = false, $default = -INF)
+	{
+		parent::__construct($name, 'MEDIUMINT');
+		$this->setUnsigned($unsigned);
+		$this->setNullable($nullable);
+		$this->setAutoIncrement($autoIncrement);
+		$this->setDefault($default);
+	}
+
+	protected function convertDefaultValue($default): int
+	{
+		return (int)$this->filterDefaultValue($default, FILTER_VALIDATE_INT);
+	}
+}
